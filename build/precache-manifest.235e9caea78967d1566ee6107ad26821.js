6442a8fe63657a16464506e47dad21b39e5a0d91
patricia2193 <51409099+patricia2193@users.noreply.github.com>
2021-05-10 11:08:00 +0300

    Add files via upload

---
+++ build/precache-manifest.235e9caea78967d1566ee6107ad26821.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "06a285e5bca934980e30c2709b804a95",
+    "url": "/patricia2193/robofriends/index.html"
+  },
+  {
+    "revision": "10b3190f9b0b7b614b87",
+    "url": "/patricia2193/robofriends/static/css/2.eb45ad44.chunk.css"
+  },
+  {
+    "revision": "a3b62324dfdb140fc8df",
+    "url": "/patricia2193/robofriends/static/css/main.6cb42e8f.chunk.css"
+  },
+  {
+    "revision": "10b3190f9b0b7b614b87",
+    "url": "/patricia2193/robofriends/static/js/2.4b6b2329.chunk.js"
+  },
+  {
+    "revision": "99bd0487192ec9e7d9ee8fbbd91ee444",
+    "url": "/patricia2193/robofriends/static/js/2.4b6b2329.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a3b62324dfdb140fc8df",
+    "url": "/patricia2193/robofriends/static/js/main.129f4fdb.chunk.js"
+  },
+  {
+    "revision": "c8699bd687ed296e4d1d",
+    "url": "/patricia2193/robofriends/static/js/runtime-main.a7ea62ee.js"
+  },
+  {
+    "revision": "12a3db8ba7f5423327bb12aed3c41977",
+    "url": "/patricia2193/robofriends/static/media/SEGA.12a3db8b.woff"
+  }
+]);
